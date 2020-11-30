d48fe3d018c72268d6a2fe770f93aea597fecbdd
Rizwan Hasan <rizwan.hasan486@gmail.com>
2020-11-30 09:48:30 +0600

    updated

--- gui/BoardLines.py
+++ gui/BoardLines.py
@@ -4,14 +4,14 @@ from OpenGL import GL
 def BoardLines():
     x: int = 0.1
     y: int = 10
-    distance: int = 3.5
     color: int = 0.35
+    thickness: int = 3.5
 
     GL.glPushMatrix()
     GL.glColor3f(color, color, color)
 
     GL.glPushMatrix()
-    GL.glTranslatef(distance, 0, 0)
+    GL.glTranslatef(thickness, 0, 0)
     GL.glBegin(GL.GL_QUADS)
     GL.glVertex2f(-x, -y)
     GL.glVertex2f(x, -y)
@@ -21,7 +21,7 @@ def BoardLines():
     GL.glPopMatrix()
 
     GL.glPushMatrix()
-    GL.glTranslatef(-distance, 0, 0)
+    GL.glTranslatef(-thickness, 0, 0)
     GL.glBegin(GL.GL_QUADS)
     GL.glVertex2f(-x, -y)
     GL.glVertex2f(x, -y)
@@ -31,7 +31,7 @@ def BoardLines():
     GL.glPopMatrix()
 
     GL.glPushMatrix()
-    GL.glTranslatef(0, distance, 0)
+    GL.glTranslatef(0, thickness, 0)
     GL.glBegin(GL.GL_QUADS)
     GL.glVertex2f(-y, -x)
     GL.glVertex2f(y, -x)
@@ -41,7 +41,7 @@ def BoardLines():
     GL.glPopMatrix()
 
     GL.glPushMatrix()
-    GL.glTranslatef(0, -distance, 0)
+    GL.glTranslatef(0, -thickness, 0)
     GL.glBegin(GL.GL_QUADS)
     GL.glVertex2f(-y, -x)
     GL.glVertex2f(y, -x)
