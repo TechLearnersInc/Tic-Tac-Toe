cb0fc7a86b8b2bc92911b674574253e72ac29a4d
Muhammad Sakib Khan Inan <sakib.khaninan@gmail.com>
2020-12-01 14:03:34 +0600

    not trying to win problem solved with greedy approach

--- MLPlayer.py
+++ MLPlayer.py
@@ -51,10 +51,11 @@ def NextMove(board_state: list):
     computer_win_max = max(computer_win.items(), key=operator.itemgetter(1))[0]
     print(user_win)
 
-    # if user_win.get(user_win_max) > computer_win.get(computer_win_max):
-    #     return user_win_max
-    # else:
-    #     return computer_win_max
+    temp_state = board_state.copy()
+    temp_state[computer_win_max] = 1
+    if GameStatus(temp_state)['Winner'] == 1:
+        return computer_win_max
+
     if user_win.get(user_win_max) <= 0.5:
         return user_win_max
     else:
@@ -64,28 +65,35 @@ def NextMove(board_state: list):
 def GameStatus(board_state: list):
     status = {'Winner': None, 'Cells': None}
 
-    if (board_state[0] == board_state[1] == board_state[2]) == (1 or 0):
+    if (board_state[0] == board_state[1] == board_state[2])  and (board_state[0] == 0 or board_state[0] == 1):
         status['Winner'] = board_state[1]
         status['Cells'] = [0, 1, 2]
-    elif (board_state[3] == board_state[4] == board_state[5]) == (1 or 0):
+    elif (board_state[3] == board_state[4] ==
+          board_state[5]) and (board_state[3] == 0 or board_state[3] == 1):
         status['Winner'] = board_state[4]
         status['Cells'] = [3, 4, 5]
-    elif (board_state[6] == board_state[7] == board_state[8]) == (1 or 0):
+    elif (board_state[6] == board_state[7] ==
+          board_state[8]) and (board_state[6] == 0 or board_state[6] == 1):
         status['Winner'] = board_state[7]
         status['Cells'] = [6, 7, 8]
-    elif (board_state[0] == board_state[3] == board_state[6]) == (1 or 0):
+    elif (board_state[0] == board_state[3] ==
+          board_state[6]) and (board_state[0] == 0 or board_state[0] == 1):
         status['Winner'] = board_state[3]
         status['Cells'] = [0, 3, 6]
-    elif (board_state[1] == board_state[4] == board_state[7]) == (1 or 0):
+    elif (board_state[1] == board_state[4] ==
+          board_state[7]) and (board_state[1] == 0 or board_state[1] == 1):
         status['Winner'] = board_state[4]
         status['Cells'] = [1, 4, 7]
-    elif (board_state[2] == board_state[5] == board_state[8]) == (1 or 0):
+    elif (board_state[2] == board_state[5] ==
+          board_state[8]) and (board_state[2] == 0 or board_state[2] == 1):
         status['Winner'] = board_state[5]
         status['Cells'] = [2, 5, 8]
-    elif (board_state[0] == board_state[4] == board_state[8]) == (1 or 0):
+    elif (board_state[0] == board_state[4] ==
+          board_state[8]) and (board_state[0] == 0 or board_state[0] == 1):
         status['Winner'] = board_state[4]
         status['Cells'] = [0, 4, 8]
-    elif (board_state[2] == board_state[4] == board_state[6]) == (1 or 0):
+    elif (board_state[2] == board_state[4] ==
+          board_state[6]) and (board_state[2] == 0 or board_state[2] == 1):
         status['Winner'] = board_state[4]
         status['Cells'] = [0, 4, 6]
 
